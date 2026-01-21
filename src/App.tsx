05bd52276af1c5994c1ddde3b0f3fcc814be7f36
dduartess <dandandsoares@gmail.com>
2026-01-21 12:12:01 -0300

    Feat: import card.tsx in main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Card } from './components/cards/card';
+import type { FoodData } from './interface/FoodData';
 
 function App() {
-  
+
+  const data: FoodData[] = [];
 
   return (
-    <div className='App'>
-
+    <div className='Container'>
+      <h1>Cardápio</h1>
+      <div className="card-grid">
+        {data.map(foodData => <Card price={foodData.price} title={foodData.title} image={foodData.image}/>)}
+      </div>
     </div>
   )
 }
