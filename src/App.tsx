d31a8b5bdef39c5e48a924386b70ac4cac25dbc7
dduartess <dandandsoares@gmail.com>
2026-01-21 12:44:13 -0300

    Feat: add new components and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css'
 import { Card } from './components/cards/card';
-import type { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
 
-  const data: FoodData[] = [];
+  const {data} = useFoodData();
 
   return (
     <div className='Container'>
       <h1>Cardápio</h1>
       <div className="card-grid">
-        {data.map(foodData => <Card price={foodData.price} title={foodData.title} image={foodData.image}/>)}
+        {data?.map(foodData =>
+           <Card 
+           price={foodData.price} 
+           title={foodData.title} 
+           image={foodData.image}
+          />
+        )}
       </div>
     </div>
   )
